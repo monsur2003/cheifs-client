c9900abe87829bdf95b2de51531f72376dcb37e6
monsur776e <monsuralam8897@gmail.com>
2023-05-02 12:12:15 +0600

    firebase setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: "AIzaSyCY4cMTy0gJvxuKKLt3vB25ZbgvzJqpwy4",
+   authDomain: "cheif-master-auth.firebaseapp.com",
+   projectId: "cheif-master-auth",
+   storageBucket: "cheif-master-auth.appspot.com",
+   messagingSenderId: "623103744796",
+   appId: "1:623103744796:web:a1ac2c144753248e12f8d9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
